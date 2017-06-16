542597d8986b28d320495a93c49bb474e66acb66
nicu-chiciuc <nicuchiciuc@gmail.com>
2017-06-16 13:17:31 +0300

    Added possibility to reset the depth.

--- src/App.js
+++ src/App.js
@@ -25,16 +25,51 @@ export default class App extends Component {
     this.setState({ extended: this.state.extended });
   };
 
+  resetLevel = () => {
+    const maxLevel = parseInt(this.resetDepth.value);
+    const root = this.state.rootObject;
+    let newExtended = [];
+
+    recursiveReset("", 0, "rootObject", root);
+
+    this.setState({ extended: newExtended });
+
+    function recursiveReset(fullPath, levelNow, name, value) {
+      if (levelNow > maxLevel) return;
+      const newFullPath = fullPath.length ? fullPath + "." + name : name;
+      newExtended.push(newFullPath);
+
+      const type = typeof value;
+
+      if (typeof value === "object") {
+        Object.keys(value).forEach(key => {
+          recursiveReset(newFullPath, levelNow + 1, key, value[key]);
+        });
+      }
+    }
+  };
+
   render() {
     return (
-      <PropertyViewer
-        global={this.state}
-        fullPath={"rootObject"}
-        name={"rootObject"}
-        value={this.state.rootObject}
-        callClick={this.handleClicks}
-        indent={0}
-      />
+      <div>
+        <div>
+          <input
+            ref={node => (this.resetDepth = node)}
+            type="text"
+            className="text"
+          />
+          <button onClick={this.resetLevel}>Reset depth</button>
+        </div>
+
+        <PropertyViewer
+          global={this.state}
+          fullPath={"rootObject"}
+          name={"rootObject"}
+          value={this.state.rootObject}
+          callClick={this.handleClicks}
+          indent={0}
+        />
+      </div>
     );
   }
 }
